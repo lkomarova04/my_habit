aece43e378dfb662ea973e025b9b77d8be433c0e
lkomarova04 <lkomarova04@yandex.ru>
2021-12-05 18:56:08 +0300

    first commit

---
+++ mysite/Habit/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('habit_show/', views.habit_show),
+    path('addhabit/', views.Add_Habit, name = 'Add_Habit'),
+	path('edithabit/', views.Edit_Habit, name = 'Edit_Habit'),
+	path('deletehabit/', views.Delete_Habit, name = 'Delete_Habit'),
+	path('modifyhabit/', views.Modify_Habit, name = 'Modify_Habit'),
+	path('chartshow/', views.Chart_show, name= 'Chart_show'),
+]
